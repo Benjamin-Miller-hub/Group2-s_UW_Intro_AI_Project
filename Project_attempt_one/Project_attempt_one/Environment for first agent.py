d492780d171df078cf0c32dc3163d58b100e28a3
Benjamin-Miller-hub <79472291+Benjamin-Miller-hub@users.noreply.github.com>
2021-11-16 22:11:38 -0700

    Modified agent 1 and 2 environment
    Modified agent 1 and 2's environments.

--- Project_attempt_one/Project_attempt_one/Environment for first agent.py
+++ Project_attempt_one/Project_attempt_one/Environment for first agent.py
@@ -22,20 +22,20 @@ def main():
     test = ConnectFour(1,2)
     #print(test.observation_space.sample())
     print(test.observation_space)
-    state,reward,done,info = test.step(3)
+    state,reward,done,info = test.step(3,1)
     new_format = test.change_format_for_neuralNet()
-    state,reward,done,info = test.step(3)
-    state,reward,done,info = test.step(3)
-    state,reward,done,info = test.step(3)
-    state,reward,done,info = test.step(2)
-    state,reward,done,info = test.step(2)
-    state,reward,done,info = test.step(3)
-    state,reward,done,info = test.step(6)
+    state,reward,done,info = test.step(3,2)
+    state,reward,done,info = test.step(3,1)
+    state,reward,done,info = test.step(3,2)
+    state,reward,done,info = test.step(2,1)
+    state,reward,done,info = test.step(2,1)
+    state,reward,done,info = test.step(3,2)
+    state,reward,done,info = test.step(3,2)
     state,reward,done,info = test.step(3)
     print("Checking")
 
 class ConnectFour(Env):
-    def __init__(self,agent_player,opponant_player):
+    def __init__(self,player_one,player_two):
         #will initialize the environment
         self.action_space = Discrete(7) # seven columns that can be used(columns 0-6)
         #self.observation_space = MultiDiscrete([3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3])#the board space is 6x7
@@ -45,10 +45,11 @@ class ConnectFour(Env):
         self.count = 0
 
         #players are 1 and 2. Zero means empty
-        self.player = agent_player
-        self.opponant = opponant_player
+        self.player_one = player_one
+        self.player_two = player_two
         #self.turnplayer = starting_player
-        #player tokens should be -1 or 1. 0 means the space is onocuppied
+        #player tokens should be 2 or 1. 
+        #0 means the space is onocuppied
         #print("cows")
 
     def get_pos_in_array(self,row,col):
@@ -57,7 +58,7 @@ class ConnectFour(Env):
            print("eh")
        return position
 
-    def step(self,action):
+    def step(self,action,player_piece):
         #need to find what the new board is
         #action determines where the chip was dropped left to right
         #0 is far left
@@ -74,7 +75,7 @@ class ConnectFour(Env):
             if i < 6:
                 array_position = self.get_pos_in_array(i,action)
                 if self.state[0][array_position] == 0:
-                    self.state[0][array_position] = self.player #places the token where it would fall
+                    self.state[0][array_position] = player_piece #places the token where it would fall
                     row_inserted = i
                     break #break so that multiple coins do not fall at the same time
             else:# this means that every position in this column is full, so return a negative reward since this is an illegal move
@@ -85,17 +86,18 @@ class ConnectFour(Env):
         #will just look at area around where the coin was added and see if that made it apart of a connect four (so if there are coins within four spots left, right,up,down, and diagonal
         if illegal_move == False:
             #check for win condition
-            reward,done = self.reward_function(row_inserted,action)
-        #change whose turn it is
-        #if self.turnplayer == self.player:
-         #   self.turnplayer = self.opponant
-        #else:
-        #    self.turnplayer = self.player
+            reward,done = self.reward_function(row_inserted,action,player_piece)
+
+
 
         info = {}
         return self.state,reward,done,info
 
-    def reward_function(self,row_inserted,action):
+    def reward_function(self,row_inserted,action,player_piece):
+        if player_piece == 1:
+            opponent_piece = 2
+        else:
+            opponent_piece = 1
         reward = 0
         done = False
         #check if four in a row
@@ -128,10 +130,13 @@ class ConnectFour(Env):
                 if current_row > -1 and current_row < 6:
                     position_in_array = self.get_pos_in_array(current_row,current_col)
                     if i == 0:
-                        if self.state[0][position_in_array] == 0:
+                        if self.state[0][position_in_array] == 0 or self.state[0][position_in_array] == opponent_piece:
                             break
-                    if self.state[0][position_in_array] == self.player:
+                    if self.state[0][position_in_array] == player_piece:
                         in_a_col+=1
+                    elif self.state[0][position_in_array] == opponent_piece:
+                        in_a_col = 0
+                        break
                 else:
                     break
 
@@ -158,11 +163,14 @@ class ConnectFour(Env):
                 if current_col > -1 and current_col < 7:
                     position_in_array = self.get_pos_in_array(current_row,current_col)
                     if i == 0:
-                        if self.state[0][position_in_array] == 0:
+                        if self.state[0][position_in_array] == 0 or self.state[0][position_in_array] == opponent_piece:
                             break
-                    if self.state[0][position_in_array] == self.player:
+                    if self.state[0][position_in_array] == player_piece:
                     #if self.state[current_row][current_col] == self.player:
                         in_a_row+=1
+                    elif self.state[0][position_in_array] == opponent_piece:
+                        in_a_row = 0
+                        break
                 else:
                     break
             if in_a_row == 4:
@@ -214,12 +222,14 @@ class ConnectFour(Env):
                 if current_col > -1 and current_col < 7 and current_row > -1 and current_row < 6:
                     position_in_array = self.get_pos_in_array(current_row,current_col)
                     if i == 0:
-                        if self.state[0][position_in_array] == 0:
+                        if self.state[0][position_in_array] == 0 or self.state[0][position_in_array] == opponent_piece:
                             break
-                    if self.state[0][position_in_array] == self.player:
+                    if self.state[0][position_in_array] == player_piece:
                     #if self.state[current_row][current_col] == self.player:
                         in_a_diag+=1
-
+                    elif self.state[0][position_in_array] == opponent_piece:
+                        in_a_diag = 0
+                        break
                 else:
                     break
             if in_a_diag == 4:
@@ -245,11 +255,14 @@ class ConnectFour(Env):
                 if current_col > -1 and current_col < 7 and current_row > -1 and current_row < 6:
                     position_in_array = self.get_pos_in_array(current_row,current_col)
                     if i == 0:
-                        if self.state[0][position_in_array] == 0:
+                        if self.state[0][position_in_array] == 0 or self.state[0][position_in_array] == opponent_piece:
                             break
-                    if self.state[0][position_in_array] == self.player:
+                    if self.state[0][position_in_array] == player_piece:
                     #if self.state[current_row][current_col] == self.player:
                         in_a_diag+=1
+                    elif self.state[0][position_in_array] == opponent_piece:
+                        in_a_diag = 0
+                        break
                 else:
                     break
             if in_a_diag == 4:
@@ -260,123 +273,6 @@ class ConnectFour(Env):
                 reward+= 10
             elif in_a_diag == 2:
                 reward+= 5
-
-
-######################################################################################################################################
-######################################################################################################################################
-#terminate if opponant wins
-        possilbe_rows = [[row_inserted-3,row_inserted-2,row_inserted-1,row_inserted],[row_inserted-2,row_inserted-1,row_inserted,row_inserted+1],[row_inserted-1,row_inserted,row_inserted+1,row_inserted+2],[row_inserted,row_inserted+1,row_inserted+2,row_inserted+3]]
-        
-        current_col = action
-        for current_rows in possilbe_rows:
-            in_a_col = 0
-            for i in range(4):
-                current_row = current_rows[i]
-                if current_row > -1 and current_row < 6:
-                    position_in_array = self.get_pos_in_array(current_row,current_col)
-                    if self.state[0][position_in_array] == self.opponant:
-                        in_a_col+=1
-                else:
-                    break
-            if in_a_col == 4:
-
-                done = True
-
-        
-
-
-#check if four in a col
-#        max_number_in_col = 0
-        current_row = row_inserted
-        groupings_of_columns = [[action-3,action-2,action-1,action],[action-2,action-1,action,action+1],[action-1,action,action+1,action+2],[action,action+1,action+2,action+3]]
-        for current_col_group in groupings_of_columns:
-            in_a_row = 0
-            for i in range(4):
-                current_col = current_col_group[i]
-                if current_col > -1 and current_col < 7:
-                    position_in_array = self.get_pos_in_array(current_row,current_col)
-                    if self.state[0][position_in_array] == self.opponant:
-                    #if self.state[current_row][current_col] == self.player:
-                        in_a_row+=1
-
-                else:
-                    break
-            if in_a_row == 4:
-                        #return 1000-self.count,True
-                done = True
-
-        #check diagonals
-      #  diag_one = [[3,0],[4,1],[5,2]]
-      #  diag_two = [[2,0],[3,1],[4,2],[5,3]]
-      #  diag_three = [[1,0],[2,1],[3,2],[4,3],[5,4]]
-      #  diag_four = [[0,0],[1,1],[2,2],[3,3],[4,4],[5,5]]
-      #  diag_five = [[0,1],[1,2],[2,3],[3,4],[4,5],[5,6]]
-      #  diag_six = [[0,2],[1,3],[2,4],[3,5],[4,6]]
-      #  diag_seven =[[0,3],[1,4],[2,5],[3,6]]
-      #  diag_eight = [[0,4],[1,5],[2,6]]
-
-
-       # diag_nine = [[2,0],[1,1],[0,2]]
-       # diag_ten = [[3,0],[2,1],[1,2],[0,3]]
-       # diag_eleven = [[4,0],[3,1],[2,2],[1,3],[0,4]]
-       # diag_twelve = [[5,0],[4,1],[3,2],[2,3],[1,4],[0,5]]
-       # diag_thirteen = [[5,1],[4,2],[3,3],[2,4],[1,5],[0,6]]
-       # diag_fourteen = [[5,2],[4,3],[3,4],[2,5],[1,6]]
-       # diag_fifteen =[[5,3],[4,4],[3,5],[2,6]]
-       # diag_sixteen = [[5,4],[4,5],[3,6]]
-       # diagonals = [diag_one,diag_two,diag_three,diag_four,diag_five,diag_six,diag_seven,diag_eight,diag_nine,diag_ten,diag_eleven,diag_twelve,diag_thirteen,diag_fourteen,diag_fifteen,diag_sixteen]
-
-        groupings_of_columns_left_to_right = [[action-3,action-2,action-1,action],[action-2,action-1,action,action+1],[action-1,action,action+1,action+2],[action,action+1,action+2,action+3]]
-        groupings_of_columns_right_to_left = [[action,action+1,action+2,action+3],[action-1,action,action+1,action+2],[action-2,action-1,action,action+1],[action-3,action-2,action-1,action]]
-        groupings_of_row_down_to_up = [[row_inserted-3,row_inserted-2,row_inserted-1,row_inserted],[row_inserted-2,row_inserted-1,row_inserted,row_inserted+1],[row_inserted-1,row_inserted,row_inserted+1,row_inserted+2],[row_inserted,row_inserted+1,row_inserted+2,row_inserted+3]]
-        groupings_of_row_up_to_down = [[row_inserted,row_inserted+1,row_inserted+2,row_inserted+3],[row_inserted-1,row_inserted,row_inserted+1,row_inserted+2],[row_inserted-2,row_inserted-1,row_inserted,row_inserted+1],[row_inserted-3,row_inserted-2,row_inserted-1,row_inserted]]
-        
-        
-######################################################################################################################################
-        #left_right, up_down
-#        max_diag = 0
-        for j in range(4):
-            in_a_diag = 0
-            current_row_group = groupings_of_row_up_to_down[j]
-            current_col_group = groupings_of_columns_left_to_right[j]
-            for i in range(4):
-                current_row = current_row_group[i]
-                current_col = current_col_group[i]
-                if current_col > -1 and current_col < 7 and current_row > -1 and current_row < 6:
-                    position_in_array = self.get_pos_in_array(current_row,current_col)
-                    if self.state[0][position_in_array] == self.opponant:
-                    #if self.state[current_row][current_col] == self.player:
-                        in_a_diag+=1
-
-                else:
-                    break
-            if in_a_diag == 4:
-                done = True
-
-
-
-######################################################################################################################################
-        #right_left,up_down
-        max_diag = 0
-        for j in range(4):
-            in_a_diag = 0
-            current_row_group = groupings_of_row_up_to_down[j]
-            current_col_group = groupings_of_columns_right_to_left[j]
-            for i in range(4):
-                current_row = current_row_group[i]
-                current_col = current_col_group[i]
-                if current_col > -1 and current_col < 7 and current_row > -1 and current_row < 6:
-                    position_in_array = self.get_pos_in_array(current_row,current_col)
-                    if self.state[0][position_in_array] == self.opponant:
-                    #if self.state[current_row][current_col] == self.player:
-                        in_a_diag+=1
-
-                else:
-                    break
-            if in_a_diag == 4:
-
-                done = True
-
 
 ######################################################################################################################################
 #######################################################################################################################################
@@ -395,11 +291,11 @@ class ConnectFour(Env):
     def render(self):
         pass
 
-    def reset(self,agent_player,other_player):
+    def reset(self,player_one,player_two):
        self.state = np.zeros([1,42]) #mimicking the observation state
        self.count = 0
-       self.player = agent_player
-       self.opponant = other_player 
+       self.player_one = agent_player
+       self.player_two = other_player 
 
        return self.state
     
