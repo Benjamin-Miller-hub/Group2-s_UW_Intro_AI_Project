ea2e9b24ce07d15e558847912b8ab4605a34e64a
nathaniel Whitham <nwhitham@uwyo.edu>
2021-12-01 04:14:03 -0800

    everything seems to be working, computer is able to play against eachother until winner is found, Hueristic seems good. consistent outcome

--- Project_attempt_one/Project_attempt_one/alpha-beta.py
+++ Project_attempt_one/Project_attempt_one/alpha-beta.py
@@ -30,93 +30,22 @@ def change_format_for_neuralNet(array_board_state):
 def check_for_win(board_state,player):
     #check if the current board state is a win for the player
     #returns true if it is, returns false if it isn't
-    #check if four in a row
-    #expects the board to be in a 6x7 state
-    if player == 1:
-        minp = 2
-    else:
-        minp = 1 
+ 
     Board= board_state
     for i in range(6): ## iterates through each spot on the board
         for j in range(7):
             if (Board[i][j] == player):
                 for y in range(3): ## checks a grid around each player token
                      for x in range(3): 
-                        if((i+(y-1) >= 0 and i+(y-1) < 6) and (j+(x-1) >= 0 and j+(x-1) < 7 ) ): # makes sure the next location is on the board
-                            offset = (i+(y-1))
-                            offsett = (j+(x-1))
-                           # print(offset,offsett)                         
-                            if (Board[i+(y-1)][j+(x-1)] == player and ( y-1 !=0 and x-1 != 0)): # checks to see if their is an opponent piece next to the origin piece
+                        if((i+(y-1) >= 0 and i+(y-1) < 6) and (j+(x-1) >= 0 and j+(x-1) < 7 ) ): # makes sure the next location is on the board                     
+                            if (Board[i+(y-1)][j+(x-1)] == player and ( y-1 !=0 or x-1 != 0)): # checks to see if player piece is next to current token
                                 if((i+2*(y-1) >= 0 and i+2*(y-1) < 6) and (j+2*(x-1) >= 0 and j+2*(x-1) < 7 ) ):# makes sure the next location is on the board 
                                     if (Board[i+2*(y-1)][j+2*(x-1)] == player): # if their is a player piece
                                         if((i+3*(y-1) >= 0 and i+3*(y-1) < 6) and (j+3*(x-1) >= 0 and j+3*(x-1) < 7 ) ): # makes sure the next location is on the board
-                                            if (Board[i+3*(y-1)][j+3*(x-1)] == player ):# if their is a piece 3 array for the origin with no pieces blocking 
-                                                #print("player at",i,j)
+                                            if (Board[i+3*(y-1)][j+3*(x-1)] == player ):# if 4 players pieces are in a row 
                                                 return True
     return False                                            
     
-    in_a_row = 0
-    
-    for current_row in range(6):
-        for current_col in range(7):
-            #print(player)
-            if board_state[current_row][current_col] == player:
-                in_a_row+=1
-                if in_a_row == 4:
-                    #return 1000-self.count,True
-                    return True
-            else:
-                in_a_row == 0
-
-    #check if four in a col
-    in_a_col = 0
-    for current_col in range(7):
-        for current_row in range(6):
-            if board_state[current_row][current_col] == player:
-                in_a_col+=1
-                if in_a_col == 4:
-                    #return 1000-self.count,True
-                    return True
-            else:
-                in_a_col == 0
-         
-                
-
-    #check diagonals
-    diag_one = [[3,0],[4,1],[5,2]]
-    diag_two = [[2,0],[3,1],[4,2],[5,3]]
-    diag_three = [[1,0],[2,1],[3,2],[4,3],[5,4]]
-    diag_four = [[0,0],[1,1],[2,2],[3,3],[4,4],[5,5]]
-    diag_five = [[0,1],[1,2],[2,3],[3,4],[4,5],[5,6]]
-    diag_six = [[0,2],[1,3],[2,4],[3,5],[4,6]]
-    diag_seven =[[0,3],[1,4],[2,5],[3,6]]
-    diag_eight = [[0,4],[1,5],[2,6]]
-
-
-    diag_nine = [[2,0],[1,1],[0,2]]
-    diag_ten = [[3,0],[2,1],[1,2],[0,3]]
-    diag_eleven = [[4,0],[3,1],[2,2],[1,3],[0,4]]
-    diag_twelve = [[5,0],[4,1],[3,2],[2,3],[1,4],[0,5]]
-    diag_thirteen = [[5,1],[4,2],[3,3],[2,4],[1,5],[0,6]]
-    diag_fourteen = [[5,2],[4,3],[3,4],[2,5],[1,6]]
-    diag_fifteen =[[5,3],[4,4],[3,5],[2,6]]
-    diag_sixteen = [[5,4],[4,5],[3,6]]
-    diagonals = [diag_one,diag_two,diag_three,diag_four,diag_five,diag_six,diag_seven,
-        diag_eight,diag_nine,diag_ten,diag_eleven,diag_twelve,diag_thirteen,diag_fourteen,diag_fifteen,diag_sixteen]
-
-    for diag_section in diagonals:
-        count = 0
-        for element in diag_section:
-            row = element[0]
-            col = element[1]
-            if board_state[row][col]  == player:
-                count = count + 1
-                if count == 4:
-                    return True
-                else:
-                    count = 0
-            #if none of these return true, then a false will be returned
-    return False
 
 def alpha_beta(Board,Player,MaxDepth): #variables where the same name as function
     #if Player == min_player: #should try to minimize its score
@@ -130,47 +59,48 @@ def max_player(Player, Board, alpha, beta, depth, MaxDepth):
         minp = 2
     else:
         minp = 1    
-    if depth == MaxDepth:
+    if depth >= MaxDepth:
         utility = get_utility(Player, Board)
-        return utility, 0
-    term, utility = is_terminal(Player,Board)
+        return (utility, 0)
+    (term, utility) = is_terminal(Player,Board)
     if term:
-        #print("TERMMINALLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL")
-        return utility, 0
+        return (utility, 0)
     v = -Infinity
     actions = get_legal_moves(Board)
-    #random.shuffle(actions) no need to randomly suffle actions
+    if len(actions) == 0 :
+        return (0,0)
     for a in range (0,len(actions)):
-        v2, a2 = min_player(minp, apply_move(Board,actions[a],Player), alpha, beta, depth+1, MaxDepth)
+        (v2, a2) = min_player(minp, apply_move(Board,actions[a],Player), alpha, beta, depth+1, MaxDepth)
         if v2 > v:
             (v, move) = v2,actions[a]
             alpha = max(alpha, v)
         if v >= beta:
-            return v, move
-    return v,move
+            return (v, move)
+    return (v,move)
 
 def min_player(Player, Board, alpha, beta, depth, MaxDepth):
     if Player == 1 :
-        minp = 2
+        maxp = 2
     else:
-        minp = 1
-    if depth == MaxDepth:
+        maxp = 1
+    if depth >= MaxDepth:
         utility = get_utility(Player, Board)
-        return utility, 0
-    term, utility = is_terminal(Player,Board)
+        return(-utility, 0) #want to return inverse value
+    (term, utility) = is_terminal(Player,Board)
     if term:
-        return utility, 0
+        return (-utility, 0) #inverse value, if player minplayer won we want to return -1
     v = Infinity
     actions = get_legal_moves(Board)
-   # random.shuffle(actions) I dont belive we need to randomly suffle the actions
+    if len(actions) == 0 :
+        return 0,0
     for a in range (0,len(actions)):
-        v2, a2 = max_player(minp, apply_move(Board,actions[a],Player), alpha, beta, depth+1, MaxDepth)
+        (v2, a2) = max_player(maxp, apply_move(Board,actions[a],Player), alpha, beta, depth+1, MaxDepth)
         if v2 < v:
             (v, move) = v2, actions[a]
             beta = min(beta, v)
         if v <= alpha:
-            return v, move
-    return v,move
+            return (v, move)       
+    return (v,move)
 
 def get_utility(Player, Board):
     if Player == 1 :
@@ -188,8 +118,8 @@ def get_utility(Player, Board):
                         total = 0 # total points for a given direction
                         # we will remove all the points for a given direction if from the origin a four in a row is impossible (of the board or piece in the way)
                         if((i+(y-1) >= 0 and i+(y-1) < 6) and (j+(x-1) >= 0 and j+(x-1) < 7 ) ): # makes sure the next location is on the board                         
-                            if ((Board[i+(y-1)][j+(x-1)] == Player and ( y-1 !=0 and x-1 != 0)) or Board[i+(y-1)][j+(x-1)] == 0 ): # checks to see if their is an opponent piece next to the origin piece
-                                if (Board[i+(y-1)][j+(x-1)] == Player and ( y-1 !=0 and x-1 != 0)):
+                            if ((Board[i+(y-1)][j+(x-1)] == Player and ( y-1 !=0 or x-1 != 0)) or Board[i+(y-1)][j+(x-1)] == 0 ): # checks to see if their is an opponent piece next to the origin piece
+                                if (Board[i+(y-1)][j+(x-1)] == Player and ( y-1 !=0 or x-1 != 0)):
                                     Row = Row + 1
                                     total = total + Row ## adds to heuristic since the player has a piece in line with the origin
                                 if((i+2*(y-1) >= 0 and i+2*(y-1) < 6) and (j+2*(x-1) >= 0 and j+2*(x-1) < 7 ) ):# makes sure the next location is on the board 
@@ -223,8 +153,8 @@ def get_utility(Player, Board):
                         total = 0 # total points for a given direction
                         # we will remove all the points for a given direction if from the origin a four in a row is impossible (of the board or piece in the way)
                         if((i+(y-1) >= 0 and i+(y-1) < 6) and (j+(x-1) >= 0 and j+(x-1) < 7 ) ): # makes sure the next location is on the board                         
-                            if ((Board[i+(y-1)][j+(x-1)] == minp and (y-1 !=0 and x-1 != 0)) or Board[i+(y-1)][j+(x-1)] == 0 ): # checks to see if their is an opponent piece next to the origin piece
-                                if (Board[i+(y-1)][j+(x-1)] == minp and (y-1 !=0 and x-1 != 0)):
+                            if ((Board[i+(y-1)][j+(x-1)] == minp and (y-1 !=0 or x-1 != 0)) or Board[i+(y-1)][j+(x-1)] == 0 ): # checks to see if their is an opponent piece next to the origin piece
+                                if (Board[i+(y-1)][j+(x-1)] == minp and (y-1 !=0 or x-1 != 0)):
                                     Row = Row + 1
                                     total = total + Row ## adds to heuristic since the player has a piece in line with the origin
                                 if((i+2*(y-1) >= 0 and i+2*(y-1) < 6) and (j+2*(x-1) >= 0 and j+2*(x-1) < 7 ) ):# makes sure the next location is on the board 
@@ -236,7 +166,7 @@ def get_utility(Player, Board):
                                             if (Board[i+3*(y-1)][j+3*(x-1)] == minp ):# if their is a piece 3 array for the origin with no pieces blocking it
                                                 Row = Row + 1
                                                 total = total + Row #add to H
-                                            elif (Board[i+3*(y-1)][j+3*(x-1)] == minp ):# if the next location in a three in a row is blocked, we remove the H gain
+                                            elif (Board[i+3*(y-1)][j+3*(x-1)] == Player ):# if the next location in a three in a row is blocked, we remove the H gain
                                                 total = 0
                                         else:
                                             total = 0 # cant make a 4 in a row in this direction
@@ -249,32 +179,29 @@ def get_utility(Player, Board):
                         else:
                              total = 0            
                         minH = minH+total                              
-                                                 
+    #global bigh
+    #global smallh 
+    #bigh = maxH            can remove comments to see utilities for Terminal state
+    #smallh = minH                                             
     return maxH - minH; 
 
-def check_if_illegal_move(board,move):
-    if board[6][move] == 0:
-        return True
-    else:
-        return False
 
-def get_legal_moves(board):
+def get_legal_moves(board): ## only 7 legal moves on a board, move can be made if top row isnt filled
     moves = []
     for i in range(7):        
         if board[0][i] == 0:
             moves.append(i)
-    #print(moves)
     return moves
 
-def is_terminal(Player, Board):
+def is_terminal(Player, Board): ## 
     if Player == 1 :
         minp = 2
     else:
         minp = 1
     if check_for_win(Board,Player):
-        return True, 1000                   # since this game is a win state just return 1 or -1
-    if check_for_win(Board,minp):
-        return True,-1000                     # get_utility(-player,Board)
+        return True, 10000                   # since this game is a win state just return 1 or -1
+    if check_for_win(Board,minp):  # i dont think these can be infinity, if it is, alpha beta wont pick a move sometimes
+        return True,-10000                    # get_utility(-player,Board)
     else:
         return False, 0
 
@@ -283,42 +210,57 @@ def apply_move(board,move,player):
     #print(move)
     newBoard = copy.deepcopy(board)
     for i in range(6):
-            if newBoard[i][move] != 0:
-                newBoard[i-1][move] = player #places the token where it would fall
+            if newBoard[i][move] != 0: #iterates in the selected col until an already placed token is found
+                newBoard[i-1][move] = player #places to token 1 above the other
                 #draw(newBoard)
                 return newBoard
-    newBoard[5][move]= player
+    newBoard[5][move]= player # if no other tokens where found, the col must be empty, so fill in bottom space
     #draw(newBoard)
     return newBoard
 
 def draw(board):
     for i in range(6):
-        print("")
         for j in range(7):
-            print(board[i][j],end="")
-    print("")
-if __name__ == '__main__':
+            print("|",board[i][j],end="") #simply draws the board
+        print("|",end="")
+        print("")
+
+if __name__ == '__main__': #idk if this will intefer with the other programs
+    
+    bigh = 0
+    smallh = 0
     board = [[0 for col in range(6+1)] for row in range(7+1)]
     for i in range(6):
         for j in range(7):
-            board[i][j]= 0    
+            board[i][j]= 0  #fills board with 0's  
     draw(board)
-    for n in range(1,7): 
+    for n in range(1,22): #iterates 22 times, the max amount of moves possible in a game
+
+        #player 1's move
         Move = alpha_beta(board,1,4)
-        print("A move ", Move)
+        print("1 move, position(", Move, ")")
         board = apply_move(board,Move,1)               
         draw(board)
+        util = get_utility(1,board)
         win,score = is_terminal(1,board)
-        print(win)
+        print("position utility:",util,"won:",win)
+        print("")
         if win :
+            print("utility for player 1 in final board:" ,bigh , " if value is 0 uncoment section in Heuristic")
+            print("utility for player 2:", smallh)
             exit()
         
-
+        #player 2's move
         Move = alpha_beta(board,2,4)     
-        print("B move ", Move)         
+        print("2 move, position(", Move, ")")       
         board = apply_move(board,Move,2)         
         draw(board)
+        util = get_utility(2,board)
         win,score = is_terminal(2,board)
+        print("position utility:",util,"won:",win)
+        print("") 
         if win :
+            print("utility for player 2 in final board:" ,bigh, " if value is 0 uncoment section in Heuristic")
+            print("utility for player 1:", smallh)
             exit()
-        print(win) 
+        
